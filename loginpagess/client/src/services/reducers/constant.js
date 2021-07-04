115afa447dc98e4b6ae99a6432714ee78a27c333
mareedubhanu <bhanumareedu222@gmail.com>
2021-07-04 11:55:37 +0530

    Delete loginpagess/client directory

--- loginpagess/client/src/services/reducers/constant.js
+++
@@ -1,3 +0,0 @@
-export const ADD_DATA="ADD_DATA";
-export const REMOVE_DATA="REMOVE_DATA";
-export const UPDATE_DATA="UPDATE_DATA";
--- loginpagess/client/src/services/reducers/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from 'redux';
-import taskActions from './reducer';
-
-const store=createStore(taskActions);
-export default store;
